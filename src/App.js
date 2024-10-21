7909afa377395fefdb6c9432864635489775d17d
ebo_shit <isakovn2005@gmail.com>
2024-10-21 20:24:41 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import logo from './assets/logo.svg';
 import './styles/App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Catalog from "./pages/Catalog";
 import Profile from "./pages/Profile";
 import Registration from "./pages/Registration";
 import Advertisement from "./pages/Advertisement";
 
 function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-          <Catalog/>
-          <Advertisement/>
-          <Profile/>
-          <Registration/>
-      </div>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <Routes>
+                        <Route path="/Catalog" element={<Catalog/>}/>
+                        <Route path="/Advertisement" element={<Advertisement/>}/>
+                        <Route path="/Profile" element={<Profile/>}/>
+                        <Route path="/Registration" element={<Registration/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
